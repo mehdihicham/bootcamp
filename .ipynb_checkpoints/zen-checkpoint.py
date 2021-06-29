0aaad3e05d2cd5369f197ada36160909e8abec52
Mehdi Hicham <mehdihicham@tsf-428-wpa-1-012.epfl.ch>
2021-06-29 15:24:20 +0200

    lessons upgrades

---
+++ .ipynb_checkpoints/zen-checkpoint.py
@@ -0,0 +1,5 @@
+#Print the first few lines of the Zen of Python
+print('Beautiful is better than ugly.')
+print('Explicit is better than implicit.')
+print('Simple is better than complex.')
+print('Complex is better than complicated.')
